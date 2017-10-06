0cd5428940627455cc99de80086062b0c1e9816a
PsyBurn <psykill@gmail.com>
2017-10-06 22:49:42 +0200

    The classical 'React' example.
    This won't compile but it's the minimal thing you usually write to get something out of React.
    And these are the things we have to implement to actaully get something on the screen.

--- app/actually_stuff.js
+++ app/actually_stuff.js
@@ -1,7 +1,11 @@
+const Hello = () => {
+  return <span className={'big-and-pretty'}>Hello webcamp!</span>;
+}
+
+ReactDOM.render(Hello(), document.getElementById('root'));
+
 // const Hello = () => {
-//   return <span className={'big-and-pretty'}>Hello webcamp!</span>;
+//   return createElement('span', {className: 'big-and-pretty' }, [`Hello Webcamp!`]);
 // }
 
-const Hello = () => {
-  return createElement('span', {className: 'big-and-pretty' }, [`Hello Webcamp!`]);
-}
+
