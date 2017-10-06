c2796a3fa2d757623d91e263abeff0406c6ce2fd
PsyBurn <psykill@gmail.com>
2017-10-06 23:13:03 +0200

    Example with element children.
    Our current code compiles.. but it's not something that we expect to be shown.

--- app/actually_stuff.js
+++ app/actually_stuff.js
@@ -16,8 +16,23 @@ function render(el, rootEl) {
   rootEl.appendChild(el);
 }
 
-const Hello = () => {
-  return createElement('span', {className: 'big-and-pretty' }, [`Hello Webcamp!`]);
+// const ListOfStuff = () => {
+//   return (
+//     <div>
+//       <h1>Wonderful world of unicorns!</h1>
+//       <img src='/assets/images/batman-unicorn.jpg' />
+//       <img src='/assets/images/faticorn.jpg' />
+//     </div>
+//   );
+// }
+
+
+const ListOfStuff = () => {
+  return createElement('div', undefined, [
+    createElement('h1', undefined, ['Wonderful world of unicorns!']),
+    createElement('img', { src: '/assets/images/batman-unicorn.jpg'}, undefined),
+    createElement('img', { src: '/assets/images/faticorn.jpg' }, undefined),
+  ]);
 }
 
-render(Hello(), document.getElementById('root'));
+render(ListOfStuff(), document.getElementById('root'));
